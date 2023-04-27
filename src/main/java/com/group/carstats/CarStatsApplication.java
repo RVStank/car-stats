a654728cd987c2e1c50cfa4aaf86e6688b1ffee8
RVStank <stankov.rostislav@gmail.com>
2023-04-27 10:22:22 +0300

    first commit

---
+++ src/main/java/com/group/carstats/CarStatsApplication.java
@@ -0,0 +1,13 @@
+package com.group.carstats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/group/carstats/CarStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.carstats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
